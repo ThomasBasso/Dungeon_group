dd25ba7ff78d5b2dc4dc6e6b42388a3139d56998
Warren <bevilacqua.warren@gmail.com>
2023-01-12 00:04:21 +0100

    Rework Gold Chest to fit the new way to manage items / weapons / armors. Gold Chest returns a armor or a weapon piece if not already own by the player.

--- src/main/java/fil/coo/adventure/entities/items/util/GoldChest.java
+++ src/main/java/fil/coo/adventure/entities/items/util/GoldChest.java
@@ -1,33 +1,89 @@
 package fil.coo.adventure.entities.items.util;
 
-import fil.coo.adventure.entities.items.Item;
 import fil.coo.adventure.utils.Constant;
 import fil.coo.adventure.entities.Player;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class GoldChest{
 
     public void isUsedBy(Player player) {
+        String piece;
         Random rand = new Random();
-        String[] armorPieces = Constant.EQUIPMENTS;
-        int n = rand.nextInt(armorPieces.length); // Génère un nombre aléatoire en fonction du nombre d'élément dans la liste
-        String piece = armorPieces[n];
+        String[] armorPieces = Constant.ARMOR;
+        String[] weaponPieces = Constant.WEAPON;
+        int a = rand.nextInt(armorPieces.length); // Génère un nombre aléatoire en fonction du nombre d'élément dans la liste
+        int w = rand.nextInt(weaponPieces.length);
+        int n = rand.nextInt(2);
+        if (n==1) {
+            piece = weaponPieces[w];
+            System.out.println("Vous avez tiré au hasard : " + piece);
 
-        System.out.println("Vous avez tiré au hasard : " + piece);
+            if (player.getWeaponEquiped().get(piece)) {
+                System.out.println("Dommage ! Vous aviez déjà cet équipement !");
+            } else {
+                int strengthBonus = getStrengthFromObject(piece);
+                System.out.println("Bravo ! Vous avez maintenant "+piece+" qui ajoute "+strengthBonus+" de force !");
+                player.getWeaponEquiped().replace(piece, true);
+                player.addStrength(strengthBonus);
+            }
 
-        // équipe l'armure au joueur si celui-ci le veut
-        Scanner sc = new Scanner(System.in);
-        String input ;
-        do {
-            System.out.println("Souhaitez-vous vous équiper de la pièce d'armure "+ piece + " ? (o/n)");
-            input = sc.nextLine().toLowerCase();
-        } while(!input.equals("o") && !input.equals("n"));
-        if (input.equals("o")) {
-            player.equipArmor(piece);
         } else {
-            System.out.println("Vous avez décidé de ne pas vous équiper de " + piece);
+            piece = armorPieces[a];
+            System.out.println("Vous avez tiré au hasard : " + piece);
+
+            if (player.getArmorEquiped().get(piece)) {
+                System.out.println("Dommage ! Vous aviez déjà cet équipement !");
+            } else {
+                int armorBonus = getArmorFromObject(piece);
+                System.out.println("Bravo ! Vous avez maintenant "+piece+" qui ajoute "+armorBonus+" d'armure !");
+                player.getArmorEquiped().replace(piece, true);
+                player.addDefense(armorBonus);
+            }
         }
+    }
+
+    public int getArmorFromObject(String piece) {
+        int armor;
+        switch (piece) {
+            case "Gauntlets":
+                armor = 4;
+                break;
+            case "Helmet":
+                armor = 6;
+                break;
+            case "Breast_plate":
+                armor = 8;
+                break;
+            case "Trousers":
+                armor = 10;
+                break;
+            case "Boots":
+                armor = 2;
+                break;
+            default:
+                armor = 0;
+                break;
+        }
+        return armor;
+    }
+
+    public int getStrengthFromObject(String piece) {
+        int strength;
+        switch (piece) {
+            case "dagger":
+                strength = 5;
+                break;
+            case "sword":
+                strength = 10;
+                break;
+            case "gun":
+                strength = 20;
+                break;
+            default:
+                strength = 0;
+                break;
+        }
+        return strength;
     }
 }
