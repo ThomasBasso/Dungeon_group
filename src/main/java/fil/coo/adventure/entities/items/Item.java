16aac6a3b73cd70398fd67f8fbc2d3c84c61b2e3
Warren <bevilacqua.warren@gmail.com>
2023-01-11 12:13:54 +0100

    Add defaults attributes for items objects with a constructor.

--- src/main/java/fil/coo/adventure/entities/items/Item.java
+++ src/main/java/fil/coo/adventure/entities/items/Item.java
@@ -3,5 +3,21 @@ package fil.coo.adventure.entities.items;
 import fil.coo.adventure.entities.Player;
 
 public abstract class Item {
+	private String name;
+	private int life, strenght, attack, armor, gold, prix;
+
+	public Item(String name, int life, int strenght, int armor, int gold, int prix) {
+		this.name = name;
+		this.life = life;
+		this.strenght = strenght;
+		this.armor = armor;
+		this.gold = gold;
+		this.prix = prix;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
 	public abstract void isUsedBy(Player player);
 }
