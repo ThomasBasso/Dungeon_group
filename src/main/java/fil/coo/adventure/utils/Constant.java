dbe27142a613e9b111a0e4d491ea7f7bd14542e7
Warren <bevilacqua.warren@gmail.com>
2023-01-11 11:16:58 +0100

    Add a constant file

---
+++ src/main/java/fil/coo/adventure/utils/Constant.java
@@ -0,0 +1,6 @@
+package fil.coo.adventure.utils;
+
+public final class Constant {
+    public static final String PLAYER_FILE = "src/main/java/fil/coo/adventure/utils/player_infos.txt";
+    public static final int MAX_ITEMS = 5;
+}
