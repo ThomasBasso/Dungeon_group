78fc3bfcf9267eded32c9186f2e105f9ede0fd87
Warren <bevilacqua.warren@gmail.com>
2023-01-12 00:07:09 +0100

    Delete weapons and armors file as they are no longer class. They are replaced in the player class with a hashmap attribute with a String as key with the name of the object and Boolean as key to know either the object is owned or not by the player.

--- src/main/java/fil/coo/adventure/entities/weapons/weapon.java
+++
@@ -1,8 +0,0 @@
-package fil.coo.adventure.entities.weapons;
-
-import fil.coo.adventure.entities.Player;
-
-public abstract class weapon {
-    public abstract void isUsedBy(Player player);
-    public abstract void isDropBy(Player player);
-}
