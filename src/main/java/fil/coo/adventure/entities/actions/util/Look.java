f645f28b9283df26d82d6db98c2de28c722f94ff
Warren <bevilacqua.warren@gmail.com>
2023-01-12 14:44:27 +0100

    Fix loot and look files. Todo: call functions from these files to manages the player look's ability.

--- src/main/java/fil/coo/adventure/entities/actions/util/Look.java
+++ src/main/java/fil/coo/adventure/entities/actions/util/Look.java
@@ -6,57 +6,59 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
-import fil.coo.adventure.entities.GameCharacters;
 import fil.coo.adventure.entities.items.Item;
 import fil.coo.adventure.entities.monsters.Monster;
 import fil.coo.adventure.places.Room;
 import fil.coo.adventure.places.directions.Direction;
+import fil.coo.adventure.places.rooms.First;
+import fil.coo.adventure.places.rooms.Second;
+import fil.coo.adventure.places.rooms.Third;
 
 public class Look {
 	protected Map<Direction, Room> neighbour;
 	private int nbRoomMax = 0;
 
-	private void addNeighbour(Direction d, First first){
-		this.neighbour.put(d, first);
-	}
-	public Set<Direction> getPossibleDirections(){
+
+	public Set<Direction> getPossibleDirections() {
 		return this.neighbour.keySet();
 	}
-	public Room getNeighbour(Direction d){
+
+	public Room getNeighbour(Direction d) {
 		return this.neighbour.get(d);
 	}
-	public void initializeNeighbour(){
-		this.addNeighbour(Direction.W, new First(this.donjon));
-		this.addNeighbour(Direction.N, new Second(this.donjon));
-		this.addNeighbour(Direction.E, new Third(this.donjon));
-		this.nbRoomMax = this.neighbour.size();
+
+	public void initializeNeighbour(Room r){
+	r.addNeighbour(Direction.W, new First(r.getDonjon()));
+	r.addNeighbour(Direction.N, new Second(r.getDonjon()));
+	r.addNeighbour(Direction.E, new Third(r.getDonjon()));
+	r.setNbRoomMax(this.neighbour.size());
 	}
-	public void chooseRoom(){
+
+	public void chooseRoom() {
 		Set<Direction> directionPossible = this.getPossibleDirections();
 		ArrayList<String> directionPossibleName = new ArrayList<>();
 		String result;
 		do {
 			System.out.println("Dans quelle salle souhaitez-vous aller ? ");
-			for (Direction d: directionPossible){
+			for (Direction d : directionPossible) {
 				directionPossibleName.add(d.name());
 				System.out.println(d.name() + " - " + d);
 			}
 			Scanner scanner = new Scanner(System.in);
 			result = scanner.nextLine().toUpperCase();
-		}
-		while (!directionPossibleName.contains(result));
+		} while (!directionPossibleName.contains(result));
 		Direction directionChoose = Direction.valueOf(result);
 		Room nextRoom = this.getNeighbour(directionChoose);
 		this.neighbour.remove(directionChoose);
 		this.execute(nextRoom);
 	}
-	public void execute(Room r){
+
+	public void execute(Room r) {
 		List<Monster> monsters = r.getMonsters();
 		List<Item> items = r.getItems();
-		List<GameCharacters> characters = r.getDeads();
 		System.out.println("Il y a " + monsters.size() + " monstres dans cette pièce.");
 	}
-	
+
 	public boolean isPossible(Room r) {
 		return true;
 	}
--- src/main/java/fil/coo/adventure/entities/actions/util/Loot.java
+++ src/main/java/fil/coo/adventure/entities/actions/util/Loot.java
@@ -2,28 +2,21 @@ package fil.coo.adventure.entities.actions.util;
 
 import java.util.List;
 
-import fil.coo.adventure.entities.GameCharacters;
 import fil.coo.adventure.entities.Player;
 import fil.coo.adventure.entities.items.Item;
 import fil.coo.adventure.places.Room;
 
 public class Loot {
 	public void execute(Room r, Player p){
-
-		List<GameCharacters> characters = r.getDeads();
 		List<Item> items = r.getItems();
 
-		for(GameCharacters gCharacters : characters){
-			p.addGold(gCharacters.getGold());
-			r.removeDead(gCharacters);
-			System.out.println("Vous avez trouvé " + gCharacters.getGold() + " golds.");
-		}
 		for(Item i :items){
 			System.out.println("Vous avez trouvé " + i.toString());
 			i.isUsedBy(p);
 			r.removeItem(i);
 		}
 	}
+
 	public boolean isPossible(Room r) {
 		return r.getMonsters().isEmpty();
 	}
