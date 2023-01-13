2845888b42a80ccdd3059413b1788f573e0c7474
Warren <bevilacqua.warren@gmail.com>
2023-01-13 11:21:30 +0100

    Add the code to start the game.

--- src/main/java/fil/coo/adventure/Main.java
+++ src/main/java/fil/coo/adventure/Main.java
@@ -1,15 +1,12 @@
 package fil.coo.adventure;
 
-import fil.coo.adventure.AdventureGame;
-import fil.coo.adventure.places.Room;
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		AdventureGame aG = new AdventureGame(Room.generateMap()); //Méthode generateMap à compléter
-		aG.toString();
-		System.out.println("Bonjour");
+	public static void main(String[] args) throws IOException {
+		AdventureGame adventureGame = AdventureGame.start();
+		adventureGame.getCurrentRoom().initializeNeighbour(adventureGame.getPlayer());
+		adventureGame.runGame();
 	}
-
 }
