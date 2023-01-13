b2a3ef640a54fde5722ee5bd06ac4d2f163ebb83
ThomasBasso <68908800+ThomasBasso@users.noreply.github.com>
2023-01-13 10:28:10 +0100

    comment every files in the items package

--- src/main/java/fil/coo/adventure/entities/items/Item.java
+++ src/main/java/fil/coo/adventure/entities/items/Item.java
@@ -6,6 +6,7 @@ public abstract class Item {
 	private String name;
 	private int life, strenght, price;
 
+	// Constructeur de tous les items
 	public Item(String name, int life, int strenght, int price) {
 		this.name = name;
 		this.life = life;
@@ -15,14 +16,17 @@ public abstract class Item {
 
 	public abstract void isUsedBy(Player player);
 
+	// Donne le nom de l'item
 	public String getName() {
 		return this.name;
 	}
 
+	// Donne les lp que l'item peut soigner
 	public int getLife() {
 		return life;
 	}
 
+	// Donne de la st que l'item peut augmenter
 	public int getStrenght() {
 		return strenght;
 	}
--- src/main/java/fil/coo/adventure/entities/items/util/GoldChest.java
+++ src/main/java/fil/coo/adventure/entities/items/util/GoldChest.java
@@ -13,31 +13,46 @@ public class GoldChest{
         Random rand = new Random();
         String[] armorPieces = Constant.ARMOR;
         String[] weaponPieces = Constant.WEAPON;
-        int a = rand.nextInt(armorPieces.length); // Génère un nombre aléatoire en fonction du nombre d'élément dans la liste
-        int w = rand.nextInt(weaponPieces.length);
-        int n = rand.nextInt(2);
+        int a = rand.nextInt(armorPieces.length); // Génère un nombre aléatoire en fonction du nombre d'élément dans la liste armorPieces
+        int w = rand.nextInt(weaponPieces.length); // Génère un nombre aléatoire en fonction du nombre d'élément dans la liste weaponPieces
+        int n = rand.nextInt(2); // tire un nombre entre 0 et 1 pour définir si le joueur tirera une arme ou une armure
+        
+        /*******
+         WEAPON
+         ********/
         if (n==1) {
+            // piece prend une valeur au hasard de la liste des weapons et on l'affiche
             piece = weaponPieces[w];
             System.out.println("Vous avez tiré au hasard : " + piece);
 
+            // Le joueur n'ajoute pas l'arme s'il l'a déjà
             if (player.getWeaponEquiped().get(piece)) {
                 System.out.println("Dommage ! Vous aviez déjà cet équipement !");
             } else {
+                // Le joueur ajoute l'arme obtenu et se l'équipe, cela augmente sa strenght du montant en fonction de l'arme obtenu
                 int strengthBonus = Fonctions.getStrengthFromObject(piece);
                 System.out.println("Bravo ! Vous avez maintenant "+piece+" qui ajoute "+strengthBonus+" de force !");
+                // Remplace l'ancienne valeur par la nouvelle à sa strength
                 player.getWeaponEquiped().replace(piece, true);
                 player.addStrength(strengthBonus);
             }
 
+        /*******
+         ARMOR
+         ********/
         } else {
+            // piece prend une valeur au hasard de la liste des armors et on l'affiche
             piece = armorPieces[a];
             System.out.println("Vous avez tiré au hasard : " + piece);
 
+            // Le joueur n'ajoute pas l'armure s'il l'a déjà
             if (player.getArmorEquiped().get(piece)) {
                 System.out.println("Dommage ! Vous aviez déjà cet équipement !");
             } else {
+                // Le joueur ajoute l'armure obtenu et se l'équipe, cela augmente sa defense du montant en fonction de l'armure obtenu
                 int armorBonus = Fonctions.getArmorFromObject(piece);
                 System.out.println("Bravo ! Vous avez maintenant "+piece+" qui ajoute "+armorBonus+" d'armure !");
+                // Remplace l'ancienne valeur par la nouvelle à sa defense
                 player.getArmorEquiped().replace(piece, true);
                 player.addDefense(armorBonus);
             }
